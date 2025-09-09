b2aa22604a0438f60bad0a24e294675dd137f275
Kla1mzZ <kla1mzz16@gmail.com>
2025-09-09 17:18:54 +0100

    added ruff and exception handler

--- src/sentiment_service/app.py
+++ src/sentiment_service/app.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from fastapi import FastAPI
 
 from .routers.predict import router as predict_router
--- src/sentiment_service/routers/predict.py
+++ src/sentiment_service/routers/predict.py
@@ -1,6 +1,6 @@
 import joblib
 from pathlib import Path
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from ..schemas.input import SentimentAnalysisInput, SentimentAnalysisOutput
 
@@ -13,10 +13,14 @@ MODEL_PATH = PROJECT_ROOT / "models" / "sentiment_pipeline.joblib"
 if not MODEL_PATH.exists():
     raise FileNotFoundError(f"Model file not found at {MODEL_PATH}")
 
-pipline = joblib.load(MODEL_PATH)
+pipeline = joblib.load(MODEL_PATH)
 
 
 @router.post("/predict", tags=["Predictions"], response_model=SentimentAnalysisOutput)
 async def predict(input: SentimentAnalysisInput) -> SentimentAnalysisOutput:
-    output = pipline.predict([input.text])
-    return SentimentAnalysisOutput(prediction=output)
+    try:
+        output = pipeline.predict([input.text])[0]
+
+        return SentimentAnalysisOutput(prediction=output)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Prediction failed")
